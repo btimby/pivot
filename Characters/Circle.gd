90a753f51304721bca7ae64a741c5145047a7b67
Ben Timby <btimby@gmail.com>
2025-06-17 11:04:18 -0400

    Sound FX

--- Characters/Circle.gd
+++ Characters/Circle.gd
@@ -1,10 +1,22 @@
 class_name Circle
 extends RigidBody2D
 
+@onready var bounce : AudioStreamPlayer2D = $Bounce
+@onready var rolling : AudioStreamPlayer2D = $Rolling
+
 var keys: Array[Globals.KeyColor] = []
+
+# func _process(delta: float) -> void:
+# 	if not self.linear_velocity.is_zero_approx() and not self.rolling.playing:
+#		self.rolling.play()
 
 func give_key(color: Globals.KeyColor) -> void:
 	self.keys.append(color)
 
 func has_key(color: Globals.KeyColor) -> bool:
 	return color in self.keys
+
+func _on_body_entered(body: Node) -> void:
+	if self.rolling.playing:
+		return
+	self.rolling.play()
